2feed1c9bb3368787a96698f55b62fc5ee9551e0
alessandrostein <alessandro@alessandrostein.com>
2015-09-14 23:25:16 -0300

    Add simple form with protected_attributes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,15 @@
-class TasksController < ApplicationController
+class TasksController < InheritedResources::Base
 
   before_action :authenticate_user!
 
   def index
     @tasks = current_user.tasks
+  end
+
+  def create
+    byebug
+    @task = Task.new params[:task]
+    @task.save
   end
 
 end
