d8a0706915005314dd951a6f1ac8b7a06bff92b5
alessandrostein <alessandro@alessandrostein.com>
2015-11-20 18:54:52 -0200

    Fix adjusts for open modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,40 +19,10 @@
 //= require sweetalert.min.js
 //= require_tree .
 
-//
-// var x = document.getElementById("demo");
-//
-// getLocation();
-//
-// function getLocation() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition, showError);
-//     } else {
-//         x.innerHTML = "Geolocation is not supported by this browser.";
-//     }
-// }
-//
-// function showPosition(position) {
-//     var latlon = position.coords.latitude + "," + position.coords.longitude;
-//
-//     var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
-//     +latlon+"&zoom=14&size=400x300&sensor=false";
-//     document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
-// }
-//
-// function showError(error) {
-//     switch(error.code) {
-//         case error.PERMISSION_DENIED:
-//             x.innerHTML = "User denied the request for Geolocation."
-//             break;
-//         case error.POSITION_UNAVAILABLE:
-//             x.innerHTML = "Location information is unavailable."
-//             break;
-//         case error.TIMEOUT:
-//             x.innerHTML = "The request to get user location timed out."
-//             break;
-//         case error.UNKNOWN_ERROR:
-//             x.innerHTML = "An unknown error occurred."
-//             break;
-//     }
-// }
+$(document).on("click touch", ".btn-do-not-account", function() {
+  $('#sign-in').modal('hide')
+});
+
+$(document).on("click touch", ".btn-has-account", function() {
+  $('#sign-up').modal('hide')
+});
